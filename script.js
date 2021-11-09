106145a2d7234f7f3d336aa22aaf51fb6e4e2c41
baxrom <baxrom>
2021-11-09 19:18:16 +0500

    js2_3dars

---
+++ script.js
@@ -0,0 +1,16 @@
+var num1 = +prompt( "1 - sonni kiriting")
+var num2 = +prompt( "2 - sonni kiriting")
+var num3 = +prompt( "3 - sonni kiriting")
+
+if (num1 < num2 && num1 > num3 ){
+    alert("O'rta qiymat " + num1    )
+}
+else if (num2 < num1 && num2 > num3 ){
+    alert("O'rta qiymat " + num2    )
+}
+else if (num3 < num2 && num3 > num1 ){
+    alert("O'rta qiymat " + num3)
+}
+else{
+    alert("error 404")
+}
